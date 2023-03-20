5c62ca4d58e026a33c25246f26cde39a17af8f42
BawerMY <bawermy123@gmail.com>
2023-03-20 18:03:29 +0100

    Update AlboView.vue

--- src/views/AlboView.vue
+++ src/views/AlboView.vue
@@ -33,7 +33,7 @@
     methods: {
       d() {
         axios
-          .get('https://mby-todo.netlify.app/'+this.page+'/info.0.json')
+          .get('/'+this.page+'/info.0.json')
           .then(response => {this.info = response.data; console.log(this.info)})
       }
     }
