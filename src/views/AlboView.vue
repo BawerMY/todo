1bcf762e67fb963c80171a6c0da7ab7e5cc923fb
BawerMY <bawermy123@gmail.com>
2023-03-20 17:07:50 +0100

    fix albo

--- src/views/AlboView.vue
+++ src/views/AlboView.vue
@@ -1,21 +1,42 @@
 <template>
-  <div>
-    <h1 class="text-center">CORS error</h1>
+  <div style="margin-right: auto; margin-left: auto;">
+    <div v-if="info" style="color: white; margin-top: 5rem;">
+      <h1>{{ info.title }}</h1>
+      <div>Date: <b>{{ (info.day<10?"0"+info.day:info.day) }}/{{ (info.month<10?"0"+info.month:info.month) }}/{{ info.year }}</b></div>
+      <div>Number: <b>{{ info.num }}</b></div>
+      <div>Image Link: <a :href="info.img">{{ info.img }}</a></div>
+    </div>
+    <div @click="d()" style="text-align: center; position: fixed; bottom: 1rem; left: 50%; transform: translateX(-50%);">
+      <v-btn style="margin-bottom: 1rem;" color="info" @click="page=''; d()">Last</v-btn>
+      <v-pagination
+        v-model="page"
+        color="secondary"
+        :length="614"
+        :total-visible="7"
+      ></v-pagination>
+    </div>
   </div>
+  
 </template>
 
 <script>
-// 90vw fix
-// overlay or koyu tonlastir boyle
-// lazy
-// style of todo and completeds
-// add bg
-// in base a tempo
-// for checkbox error check video => multi v-model connection(maybe for lists)
-// maybe add multi name select in filter of achive search
-// title-subtitle card error
-// home archive arasi geciste data saklansin ama logout olursa gitsin
-// fix responsivity
-// maybe fix username filter width
+  import axios from 'axios'
+  export default {
+    name: 'AlboApp',
+    data() {
+      return {
+        page: 100,
+        info: null,
+      }
+    },
+    created() {this.d()},
+    methods: {
+      d() {
+        axios
+          .get('http://localhost:8080/'+this.page+'/info.0.json')
+          .then(response => (this.info = response.data))
+      }
+    }
+  }
 // maybe for performance optimization v-if in route-viewer for events
 </script>
